70b2069d7dac19db4e3b7e29337f7b4f827556e4
judibec <juanchopolo6954@gmail.com>
2022-04-13 10:23:14 -0500

    base de datos y pagina login

--- src/main/java/edu/eci/cvds/gestor/guice/GuiceContextListener.java
+++ src/main/java/edu/eci/cvds/gestor/guice/GuiceContextListener.java
@@ -1,4 +1,17 @@
 package edu.eci.cvds.gestor.guice;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import edu.eci.cvds.gestor.persistence.*;
+import edu.eci.cvds.gestor.persistence.mybatis.*;
+import edu.eci.cvds.gestor.services.GestorServices;
+import edu.eci.cvds.gestor.services.impl.GestorServicesImpl;
+import org.mybatis.guice.XMLMyBatisModule;
+import org.mybatis.guice.datasource.helper.JdbcHelper;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
 public class GuiceContextListener {
 }
--- src/main/java/edu/eci/cvds/gestor/services/impl/GestorServicesImpl.java
+++ src/main/java/edu/eci/cvds/gestor/services/impl/GestorServicesImpl.java
@@ -1,6 +1,33 @@
 package edu.eci.cvds.gestor.services.impl;
 
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import edu.eci.cvds.gestor.persistence.*;
 import edu.eci.cvds.gestor.services.GestorServices;
 
+
+
+@Singleton
 public class GestorServicesImpl implements GestorServices {
+
+    @Inject
+    private AdminDAO adminDAO;
+
+    @Inject
+    private BoardDAO boardDAO;
+
+    @Inject
+    private BookDAO bookDAO;
+
+    @Inject
+    private CommunityDAO communityDAO;
+
+    @Inject
+    private ComputerDAO computerDAO;
+
+    @Inject
+    private ReservationDAO reservationDAO;
+
+    @Inject
+    private RoomDAO roomDAO;
 }
