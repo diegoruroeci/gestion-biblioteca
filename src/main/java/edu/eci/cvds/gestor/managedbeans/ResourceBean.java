5af341443d06d70a530f250a791f83190066cf95
diegoruro <diegofernandoruizrojas@gmail.com>
2022-04-19 23:20:30 -0500

    servicios de usuario creado

--- src/main/java/edu/eci/cvds/gestor/managedbeans/ResourceBean.java
+++ src/main/java/edu/eci/cvds/gestor/managedbeans/ResourceBean.java
@@ -3,6 +3,7 @@ package edu.eci.cvds.gestor.managedbeans;
 import com.google.inject.Inject;
 import edu.eci.cvds.gestor.entities.Resource;
 import edu.eci.cvds.gestor.services.GestorServices;
+import edu.eci.cvds.gestor.services.UserServices;
 
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
@@ -12,6 +13,10 @@ import java.util.List;
 @ManagedBean(name="ResourceBean")
 @ApplicationScoped
 public class ResourceBean extends BasePageBean{
+
+
+    @Inject
+    private UserServices userServices;
 
     @Inject
     private GestorServices gestorServices;
